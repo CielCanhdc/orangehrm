1238fbaef0b084b694e761af6435018e9b5b3d41
congcanh.dinh <congcanh.dinh@ved.com.vn>
2025-10-09 15:56:37 +0700

    feat: attach screenshot failure to html report

--- conftest.py
+++ conftest.py
@@ -1,4 +1,3 @@
-import os
 import pytest
 import logging
 from datetime import datetime
@@ -7,6 +6,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from config import Config, Grid
 from utils.constant import ErrorCode
+from utils.screenshot import screenshot_failure
 
 
 @pytest.fixture(scope='function', params=['chrome'])
@@ -41,6 +41,7 @@ def driver_for_grid(request):
 
 
 def pytest_configure(config):
+    """Config ghi log vào file"""
     today = datetime.now().strftime("%Y-%m-%d")
     log_filename = f"report/logs_{today}.log"
     logging.basicConfig(
@@ -51,3 +52,53 @@ def pytest_configure(config):
         datefmt="%Y-%m-%d %H:%M:%S"
     )
 
+
+class MemoryLogHandler(logging.Handler):
+    """Collect logs in memory for each test"""
+    def __init__(self):
+        super().__init__()
+        self.logs = []
+
+    def emit(self, record):
+        msg = self.format(record)
+        self.logs.append(msg)
+
+
+@pytest.fixture(autouse=True)
+def capture_logs_per_test(request):
+    """Trích xuất Log steps ra, push lên googlesheet realtime"""
+    handler = MemoryLogHandler()
+    formatter = logging.Formatter("%(asctime)s %(levelname)s:> %(message)s",
+                                  datefmt="%Y-%m-%d %H:%M:%S")
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+
+    yield
+
+    # # --- Teardown section ---
+    # print("\n------ Captured logs (from logger) ------")
+    # for line in handler.logs:
+    #     print(line)
+    print(handler.logs)
+    logger.removeHandler(handler)
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """
+    Attact ảnh chụp màn hình tại thời điểm fail vào report html
+    """
+    outcome = yield
+    report = outcome.get_result()
+
+    pytest_html = item.config.pluginmanager.getplugin("html")
+    extra = getattr(report, "extra", [])
+    if report.when == "call":
+        xfail = hasattr(report, "wasxfail")
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            screenshot_path = screenshot_failure(item)
+            extra.append(pytest_html.extras.image(screenshot_path, '')) if screenshot_path else None
+
+        report.extra = extra
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,27 +1,31 @@
 import pytest
 from utils.jiraa import jiraa
 from pom.B_bases.base_test import BaseTest
-
+from utils import assertion
 
 class TestLogin(BaseTest):
 
     @jiraa(test_id='LOGIN-01')
     @pytest.mark.parametrize('username, passwd', [('Admin', 'admin123')])
     def test_login_successfully(self, username, passwd):
-        (self.login
-            .enter_username(username)
-            .enter_password(passwd)
-            .click_login()
-            .verify_login_successfully()
-         )
+        # (self.login
+        #     .enter_username(username)
+        #     .enter_password(passwd)
+        #     .click_login()
+        #     .verify_login_successfully()
+        #  )
+        assertion.equal(1,1, 'test1')
+        self.login.goto('https://facebook.com')
+
 
     @jiraa(test_id='LOGIN-02')
     @pytest.mark.parametrize('username, passwd, message', [('Admin', 'wrong pw', 'Invalid credential')])
     def test_login_invalid(self, username, passwd, message):
-        (self.login
-            .enter_username(username)
-            .enter_password(passwd)
-            .click_login()
-            .verify_login_error_message(message)
-         )
-
+        # (self.login
+        #     .enter_username(username)
+        #     .enter_password(passwd)
+        #     .click_login()
+        #     .verify_login_error_message(message)
+        #  )
+        assertion.equal(1, 2, 'test2')
+        self.login.goto('https://facebook.com')
